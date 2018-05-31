b7e79d80c29d2ed8bcb0b88ef1d1b80c925ba0c9
Iván Rivera <rbivan@gmail.com>
2018-05-31 08:37:12 +0200

    Primera versión
    Esta es mi primera versión

---
+++ PruebaGit/src/main/java/com/sunion/java/PruebaGit/App.java
@@ -0,0 +1,13 @@
+package com.sunion.java.PruebaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
